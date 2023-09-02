2053ba3f1012d4790a36bc53aa103040d754a973
Amelia-Pencak <amelia.pencak@gmail.com>
2023-09-02 14:35:24 +0200

    loader

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,18 +1,29 @@
 import { HTMLAttributes } from "react";
 import { Helmet } from "react-helmet-async";
+import { Loader } from "./Loader";
+import "./Loader.css";
 
-export interface ContentProps extends HTMLAttributes<HTMLElement>{
+export interface ContentProps extends HTMLAttributes<HTMLElement> {
      title: string;
+     loading?: boolean;
 }
 
 export const Content = (props: ContentProps) => {
-     const { children, title,  ...other} = props;
-     return <main className="p-10" {...other}> 
-     
-     <Helmet>
-        <title>{ title }</title>
-      </Helmet>
-     
-     {children}
+     const { children, title, loading = false, ...other } = props;
+     return <main className=" p-10" {...other}>
+
+          <Helmet>
+               <title>{title}</title>
+          </Helmet>
+          {
+               loading && (
+                    <div className="fixed top-0 left-0 w-full h-full z-50 backdrop-opacity-40 bg-[#f6f5f1]/75 flex items-center justify-center">
+                         <Loader className="loader" />
+                    </div>
+
+               )
+          }
+
+          {children}
      </main>
 };
---
+++ src/components/Loader.tsx
@@ -0,0 +1,8 @@
+import clsx from "clsx";
+import { HtmlHTMLAttributes } from "react";
+// import "./Loader.css";
+
+export const Loader = ({ className, ...other }: Omit<HtmlHTMLAttributes<HTMLSpanElement>, "children">) => {
+     return <span className={clsx(className, "Loader-root")} {...other} />
+
+};
