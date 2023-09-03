d290954f733aa1e54e71b96f022917b86974dc88
Amelia-Pencak <amelia.pencak@gmail.com>
2023-09-03 20:46:42 +0200

    homePage (loading)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,26 @@
 import { toast } from "react-toastify";
 import { Content } from "../components";
+import 'react-toastify/dist/ReactToastify.css'
 
 export const HomePage = () => {
      const notify = () => toast("Wow so easy!");
 
-     return <Content title="Home page">
+    
+  
+     return <Content title="Home page" loading={false}>
           <button onClick={notify}>Notify!</button>
           Home page
+          <p>Home Page</p>
+          <p>Home Page</p>
+          <p>Home Page</p>
+          <p>Home Page</p>
+          <p>Home Page</p>
+          <p>Home Page</p>
+          <p>Home Page</p>
+          <p>Home Page</p>
+          <p>Home Page</p>
+          <p>Home Page</p>
+          <p>Home Page</p>
+          
      </Content>
 };
